c2d26ac1c4fb81d22d3c37932dc53197ed499c0f
Nikita Moshensky <nmo@dixa.com>
2022-11-06 23:56:23 +0200

    save

--- src/main/scala/sandbox/Main.scala
+++ src/main/scala/sandbox/Main.scala
@@ -1,6 +1,6 @@
 package sandbox
 
-import cats.implicits._
+// import cats.implicits._
 import json.Json
 import json.JsonWriter
 import json.JsonSyntax._
@@ -8,8 +8,14 @@ import json.JsonWriterInstances._
 import printable.Printable
 import printable.PrintableSyntax._
 
+import cats.Show
+import cats.syntax.show._
+// import cats.instances.int._
+import java.util.Date
+
+
 object Main extends App {
-  println("Hello " |+| "Cats!")
+  // println("Hello " |+| "Cats!")
   val dave = Person("Dave", "dave@example.com")
   println(dave)
 
@@ -24,4 +30,12 @@ object Main extends App {
   val cat = Cat("Kasmi", 1, "striped")
   Printable.print(cat)
   cat.print
+
+  val showInt = Show.apply[Int]
+  val showStr = Show.apply[String]
+  println(showInt.show(7))
+  println(showStr.show("xxx"))
+
+  implicit val dateShow0: Show[Date] = Show.show(x => s"${x.getTime}ms since the epoch.")
+  println(new Date().show)
 }
