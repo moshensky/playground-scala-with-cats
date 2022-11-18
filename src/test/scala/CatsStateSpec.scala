623703e0b1930fe47db77079846ac4636435ddd0
Nikita Moshensky <nmo@dixa.com>
2022-11-18 23:25:20 +0200

    save

--- src/test/scala/CatsStateSpec.scala
+++ src/test/scala/CatsStateSpec.scala
@@ -3,9 +3,55 @@ import munit.FunSuite
 
 class CatsStateSpec extends FunSuite {
 
-  val getDemo = State.get[Int]
+  type CalcState[A] = State[List[Int], A]
+
+  def someTransformation(num: Int): CalcState[Int] = State { oldStack =>
+    (num :: oldStack, num)
+  }
+  def someCalculation(f: (Int, Int) => Int): CalcState[Int] = State {
+    case b :: a :: tail => (tail, f(a, b))
+    case _              => ???
+  }
+
+  def evalOne(sym: String): CalcState[Int] = {
+    sym match {
+      case "+" => someCalculation(_ + _)
+      case x   => someTransformation(x.toInt)
+    }
+  }
 
   test("State") {
-      // val step1 = State[Int, String]{}
+    val step1 = State[Int, String] { num =>
+      val ans = num * 2
+      (ans, s"Result of step1: $ans")
+    }
+
+    val step2 = State[Int, String] { num =>
+      val ans = num * 2
+      (ans, s"Result of step2: $ans")
+    }
+
+    val both = for {
+      a <- step1
+      b <- step2
+    } yield (a, b)
+
+    val (state, result) = both.run(20).value
+    println(state)
+    println(result)
+  }
+
+  test("evalOne") {
+    assertEquals(evalOne("42").runA(Nil).value, 42)
+  }
+
+  test("evalOne with op") {
+    val program = for {
+      _ <- evalOne("1")
+      _ <- evalOne("2")
+      ans <- evalOne("+")
+    } yield ans
+
+    assertEquals(program.runA(Nil).value, 3)
   }
 }
