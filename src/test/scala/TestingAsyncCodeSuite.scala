681065658d25dbc8755ab66e8d9c7095f1a787e9
Nikita Moshensky <nmo@dixa.com>
2022-11-21 17:02:16 +0200

    save

--- src/test/scala/TestingAsyncCodeSuite.scala
+++ src/test/scala/TestingAsyncCodeSuite.scala
@@ -1,27 +1,25 @@
 import munit.FunSuite
 import scala.concurrent.Future
-// import scala.concurrent.ExecutionContext.Implicits.global
-import cats.Traverse
 import cats.Id
 import cats.Applicative
+import cats.syntax.all._
 
 trait UptimeClient[F[_]] {
   def getUptime(hostname: String): F[Int]
 }
 
 class UptimeService[F[_]: Applicative](client: UptimeClient[F]) {
-  import cats.syntax.functor._
-  def getTotalUptime(hostnames: List[String]): F[Int] = {
-    val xxx = Traverse[List].traverse[F, String, Int](hostnames)(client.getUptime).map(_.sum)
-    xxx
-  }
+  def getTotalUptime(hostnames: List[String]): F[Int] =
+    hostnames.traverse(client.getUptime).map(_.sum)
+
 }
 
 class RealUptimeClient extends UptimeClient[Future] {
   override def getUptime(hostname: String): Future[Int] = ???
 }
 
-class TestUptimeClient(val hostsMap: Map[String, Int]) extends UptimeClient[Id] {
+class TestUptimeClient(val hostsMap: Map[String, Int])
+    extends UptimeClient[Id] {
 
   override def getUptime(hostname: String): Int = hostname match {
     case "host1" => 10
