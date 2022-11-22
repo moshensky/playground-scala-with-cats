4c733e74e008e700103d95e81b088e095a1c3851
Nikita Moshensky <nmo@dixa.com>
2022-11-22 12:47:44 +0200

    save

--- src/test/scala/Ch10DataValidationSuite.scala
+++ src/test/scala/Ch10DataValidationSuite.scala
@@ -1,5 +1,8 @@
 import munit.FunSuite
 import cats.Semigroup
+import cats.data.Validated
+import cats.data.Validated._
+import cats.syntax.apply._
 
 final case class CheckF[E, A](f: A => Either[E, A]) {
   def apply(a: A): Either[E, A] = f(a)
@@ -18,24 +21,19 @@ final case class CheckF[E, A](f: A => Either[E, A]) {
 sealed trait Check[E, A] {
   import Check._
   def and(that: Check[E, A]): Check[E, A] = And(this, that)
-  def apply(a: A)(implicit s: Semigroup[E]): Either[E, A] = this match {
-    case And(left, right) =>
-      (left(a), right(a)) match {
-        case (Left(e1), Left(e2)) => Left(s.combine(e1, e2))
-        case (Left(e), Right(_))  => Left(e)
-        case (Right(_), Left(e))  => Left(e)
-        case (Right(_), Right(_)) => Right(a)
-      }
-    case Pure(f) => f(a)
+
+  def apply(a: A)(implicit s: Semigroup[E]): Validated[E, A] = this match {
+    case And(left, right) => (left(a), right(a)).mapN((_, _) => a)
+    case Pure(f)          => f(a)
   }
 }
 
 object Check {
   final case class And[E, A](left: Check[E, A], right: Check[E, A])
       extends Check[E, A]
-  final case class Pure[E, A](f: A => Either[E, A]) extends Check[E, A]
+  final case class Pure[E, A](f: A => Validated[E, A]) extends Check[E, A]
 
-  def pure[E, A](f: A => Either[E, A]) = Pure(f)
+  def pure[E, A](f: A => Validated[E, A]) = Pure(f)
 }
 
 class Ch10DataValidationSuite extends FunSuite {
@@ -53,13 +51,13 @@ class Ch10DataValidationSuite extends FunSuite {
 
   test("Check") {
     val a: Check[List[String], Int] = Check.pure { x =>
-      if (x > 2) Right(x) else Left(List("Must be > 2"))
+      if (x > 2) Valid(x) else Invalid(List("Must be > 2"))
     }
     val b = Check.pure[List[String], Int] { x =>
-      if (x < -2) Right(x) else Left(List("Must be < -2"))
+      if (x < -2) Valid(x) else Invalid(List("Must be < -2"))
     }
     val check = a and b
-    assertEquals(check(5), Left(List("Must be < -2")))
-    assertEquals(check(0), Left(List("Must be > 2", "Must be < -2")))
+    assertEquals(check(5), Invalid(List("Must be < -2")))
+    assertEquals(check(0), Invalid(List("Must be > 2", "Must be < -2")))
   }
 }
