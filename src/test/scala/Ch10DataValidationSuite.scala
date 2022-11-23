59cf749bacf8a49ea20872901a0b9be545a4392f
Nikita Moshensky <nmo@dixa.com>
2022-11-23 11:30:15 +0200

    save

--- src/test/scala/Ch10DataValidationSuite.scala
+++ src/test/scala/Ch10DataValidationSuite.scala
@@ -18,22 +18,83 @@ final case class CheckF[E, A](f: A => Either[E, A]) {
     }
 }
 
-sealed trait Check[E, A] {
-  import Check._
-  def and(that: Check[E, A]): Check[E, A] = And(this, that)
+sealed trait CheckT[E, A] {
+  import CheckT._
+  def and(that: CheckT[E, A]): CheckT[E, A] = And(this, that)
+
+  def or(that: CheckT[E, A]): CheckT[E, A] = Or(this, that)
 
   def apply(a: A)(implicit s: Semigroup[E]): Validated[E, A] = this match {
+    case Or(left, right) =>
+      (left(a), right(a)) match {
+        case (Invalid(e1), Invalid(e2)) => Invalid(Semigroup[E].combine(e1, e2))
+        case _                          => Valid(a)
+      }
     case And(left, right) => (left(a), right(a)).mapN((_, _) => a)
     case Pure(f)          => f(a)
   }
 }
 
-object Check {
-  final case class And[E, A](left: Check[E, A], right: Check[E, A])
-      extends Check[E, A]
-  final case class Pure[E, A](f: A => Validated[E, A]) extends Check[E, A]
+object CheckT {
+  final case class And[E, A](left: CheckT[E, A], right: CheckT[E, A])
+      extends CheckT[E, A]
+  final case class Or[E, A](left: CheckT[E, A], right: CheckT[E, A])
+      extends CheckT[E, A]
+  final case class Pure[E, A](f: A => Validated[E, A]) extends CheckT[E, A]
 
   def pure[E, A](f: A => Validated[E, A]) = Pure(f)
+}
+
+sealed trait Predicate[E, A] {
+  import Predicate._
+  def and(that: Predicate[E, A]): Predicate[E, A] = And(this, that)
+  def or(that: Predicate[E, A]): Predicate[E, A] = Or(this, that)
+  def apply(a: A)(implicit s: Semigroup[E]): Validated[E, A] = this match {
+    case And(left, right) => (left(a), right(a)).mapN((_, _) => a)
+    case Pure(f)          => f(a)
+    case Or(left, right) =>
+      (left(a), right(a)) match {
+        case (Invalid(e1), Invalid(e2)) => Invalid(s.combine(e1, e2))
+        case _                          => Valid(a)
+      }
+  }
+}
+
+object Predicate {
+  final case class And[E, A](left: Predicate[E, A], right: Predicate[E, A])
+      extends Predicate[E, A]
+  final case class Or[E, A](left: Predicate[E, A], right: Predicate[E, A])
+      extends Predicate[E, A]
+  final case class Pure[E, A](f: A => Validated[E, A]) extends Predicate[E, A]
+
+  def pure[E, A](f: A => Validated[E, A]) = Pure(f)
+}
+
+sealed trait Check[E, A, B] {
+  import Check._
+
+  def apply(in: A)(implicit s: Semigroup[E]): Validated[E, B]
+
+  def map[C](f: B => C): Check[E, A, C] = Map[E, A, B, C](this, f)
+
+}
+
+object Check {
+  private final case class Map[E, A, B, C](check: Check[E, A, B], f: B => C)
+      extends Check[E, A, C] {
+
+    override def apply(in: A)(implicit s: Semigroup[E]): Validated[E, C] = check(in).map(f)
+
+  }
+
+  private final case class Pure[E, A](pred: Predicate[E, A])
+      extends Check[E, A, A] {
+
+    override def apply(in: A)(implicit s: Semigroup[E]): Validated[E, A] = pred(in)
+
+  }
+
+  def apply[E, A](pred: Predicate[E, A]): Check[E, A, A] = Pure(pred)
 }
 
 class Ch10DataValidationSuite extends FunSuite {
@@ -50,14 +111,26 @@ class Ch10DataValidationSuite extends FunSuite {
   }
 
   test("Check") {
-    val a: Check[List[String], Int] = Check.pure { x =>
+    val a: CheckT[List[String], Int] = CheckT.pure { x =>
       if (x > 2) Valid(x) else Invalid(List("Must be > 2"))
     }
-    val b = Check.pure[List[String], Int] { x =>
+    val b = CheckT.pure[List[String], Int] { x =>
       if (x < -2) Valid(x) else Invalid(List("Must be < -2"))
     }
     val check = a and b
     assertEquals(check(5), Invalid(List("Must be < -2")))
+    assertEquals(check(0), Invalid(List("Must be > 2", "Must be < -2")))
+  }
+
+  test("Check or") {
+    val a: CheckT[List[String], Int] = CheckT.pure { x =>
+      if (x > 2) Valid(x) else Invalid(List("Must be > 2"))
+    }
+    val b = CheckT.pure[List[String], Int] { x =>
+      if (x < -2) Valid(x) else Invalid(List("Must be < -2"))
+    }
+    val check = a or b
+    assertEquals(check(5), Valid(5))
     assertEquals(check(0), Invalid(List("Must be > 2", "Must be < -2")))
   }
 }
